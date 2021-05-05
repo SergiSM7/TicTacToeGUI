35aeaec35605cf4fd6a15e79b7e2e3443de6b3a2
SergiSM7 <65180552+SergiSM7@users.noreply.github.com>
2021-05-05 18:36:47 +0200

    Commit branca vertical

--- src/TicTacToeGUI.java
+++ src/TicTacToeGUI.java
@@ -152,6 +152,20 @@ public class TicTacToeGUI extends JFrame {
 		   JOptionPane.showMessageDialog(null, "El jugador " +
 		     jugadorActual + " ha guanyat!");
 		   tenimGuanyador = true;
+		 } else if (taulell[0][0].getText().equals(jugadorActual) && 
+				    taulell[1][0].getText().equals(jugadorActual) && 
+				    taulell[2][0].getText().equals(jugadorActual) 
+				    || 
+				    taulell[0][1].getText().equals(jugadorActual) && 
+				    taulell[1][1].getText().equals(jugadorActual) && 
+				    taulell[2][1].getText().equals(jugadorActual) 
+				    || 
+				    taulell[0][2].getText().equals(jugadorActual) && 
+				    taulell[1][2].getText().equals(jugadorActual) && 
+				    taulell[2][2].getText().equals(jugadorActual)) {
+		   JOptionPane.showMessageDialog(null, "El jugador " +
+		     jugadorActual + " ha guanyat vertical!");
+		   tenimGuanyador = true;
 		 }
 	   
 	   
